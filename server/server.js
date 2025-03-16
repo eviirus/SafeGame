a4943ee3e6cf4fca86d6fae6a08234f2eb013aee
eviirus <evaldas.zuralovic@gmail.com>
2025-03-16 14:40:41 +0200

    inital commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
