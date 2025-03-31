8c8f6d28a5c5d254678fa52aa7e9c89a86dbeb7d
eviirus <evaldas.zuralovic@gmail.com>
2025-03-31 19:10:10 +0300

    added frontend part to handle generate results #13

--- frontend/src/components/input-fields/handleSubmit.jsx
+++ frontend/src/components/input-fields/handleSubmit.jsx
@@ -1,30 +1,63 @@
 import axios from "axios";
 
-export const handleSubmit = (inputValue, fileName, file, inputType) => {
-  console.log("form submitted with input:", inputValue, "file:", fileName);
-  console.log(inputType);
-
+export const handleSubmit = (
+  inputValue,
+  fileName,
+  file,
+  inputType,
+  handleResultReceived,
+  handleGeneratedResult
+) => {
+  handleResultReceived(false);
+  handleGeneratedResult([]);
   if (inputType === "text") {
-    handleTextInput(inputValue);
+    handleTextInput(inputValue, handleResultReceived, handleGeneratedResult);
   } else if (inputType === "link") {
     handleLinkInput(inputValue);
   } else if (inputType === "file") {
-    handleFileInput(fileName, file);
+    handleFileInput(
+      fileName,
+      file,
+      handleResultReceived,
+      handleGeneratedResult
+    );
   }
 };
 
-const handleTextInput = async (inputValue) => {
+const handleTextInput = async (
+  inputValue,
+  handleResultReceived,
+  handleGeneratedResult
+) => {
+  const receivedFormattedText = await prepareTextInput(inputValue);
+
+  if (receivedFormattedText !== null)
+    generateResultFromText(
+      receivedFormattedText,
+      handleResultReceived,
+      handleGeneratedResult
+    );
+};
+
+const prepareTextInput = async (text) => {
   const MIN_INPUT_LENGTH = 1000;
 
-  if (inputValue.length < MIN_INPUT_LENGTH) {
+  if (text.length < MIN_INPUT_LENGTH) {
     alert("Patikrinkite ar įklijavote pilną privatumo politikos tekstą");
   } else {
     try {
-      await axios.post("http://localhost:5000/endpoints/processText", {
-        text: inputValue,
-      });
+      const formattedText = await axios.post(
+        "http://localhost:5000/endpoints/processText",
+        {
+          text: text,
+        }
+      );
+
+      return formattedText.data;
     } catch (error) {
       alert("Įvyko klaida įkeliant tekstą į serverį");
+
+      return null;
     }
   }
 };
@@ -39,29 +72,77 @@ const handleLinkInput = (inputValue) => {
   }
 };
 
-const handleFileInput = async (fileName, file) => {
+const handleFileInput = async (
+  fileName,
+  file,
+  handleResultReceived,
+  handleGeneratedResult
+) => {
   const fileExtension = fileName.toLowerCase().split(".").pop();
 
-  if (fileExtension === "pdf") {
-    const formData = new FormData();
-    formData.append("file", file);
-
-    try {
-      await axios.post(
-        "http://localhost:5000/endpoints/convertFile",
-        formData,
-        {
-          headers: { "Content-Type": "multipart/form-data" },
-        }
-      );
-    } catch (error) {
-      if (error.response) {
-        alert(error.response.data.message);
-      } else {
-        alert("Įvyko klaida įkeliant failą į serverį");
-      }
-    }
-  } else {
+  if (fileExtension !== "pdf") {
     alert("Tik .pdf failai yra leidžiami");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const receivedText = await convertFile(formData);
+
+  if (receivedText !== null)
+    generateResultFromText(
+      receivedText.data,
+      handleResultReceived,
+      handleGeneratedResult
+    );
+};
+
+const convertFile = async (formData) => {
+  try {
+    const convertedText = await axios.post(
+      "http://localhost:5000/endpoints/convertFile",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+
+    return convertedText.data;
+  } catch (error) {
+    if (error.response) {
+      alert(error.response.data.message);
+    } else {
+      alert("Įvyko klaida įkeliant failą į serverį");
+    }
+
+    return null;
+  }
+};
+
+const generateResultFromText = async (
+  text,
+  handleResultReceived,
+  handleGeneratedResult
+) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/endpoints/generateResultFromText",
+      {
+        text: text,
+      }
+    );
+
+    if (response && response.data.success === false) {
+      alert(
+        "Įvyko netikėta klaida, bandykite pateikti privatumo politiką dar kartą."
+      );
+      handleResultReceived(false);
+    } else {
+      handleResultReceived(true);
+      handleGeneratedResult(response.data);
+    }
+  } catch (error) {
+    alert("Įvyko klaida generuojant rezultatą");
   }
 };
